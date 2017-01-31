bd13263e285608ffc658c78a9d8e26f8af7eae7c
igornobre <ferreiranobre@outlook.com>
2017-01-31 21:58:00 -0200

    Initial commit

---
+++ app/src/main/java/com/learnminer/ichat_alura/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.learnminer.ichat_alura.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.learnminer.ichat_alura.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/learnminer/ichat_alura/adapter/MensagemAdapter.java
@@ -0,0 +1,66 @@
+package com.learnminer.ichat_alura.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by igornobre on 31/01/17.
+ */
+
+public class MensagemAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
